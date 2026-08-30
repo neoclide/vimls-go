751d5aa9a538911028355b76e21a610e75f55843
Qiming Zhao <chemzqm@gmail.com>
2026-08-30 23:01:37 +0800

    E1394: cover lowercase type alias names

--- internal/syntax/declarations_test.go
+++ internal/syntax/declarations_test.go
@@ -579,6 +579,18 @@ func TestVim9TypeAlias(t *testing.T) {
 	}
 }
 
+func TestVim9TypeAliasLowercaseNameDiagnostic(t *testing.T) {
+	file := Parse("vim9script\ntype index = number\nvar after = 1\n")
+	if len(file.Diagnostics) != 1 || file.Diagnostics[0].Code != "vim/E1394" || file.Diagnostics[0].Message != "Type name must start with an uppercase letter: index = number" || file.Text(file.Diagnostics[0].Span) != "index" {
+		t.Fatalf("diagnostics = %#v", file.Diagnostics)
+	}
+	alias := file.Commands[1].TypeAlias
+	if alias == nil || file.Text(alias.Name) != "index" || alias.Type == nil || alias.Type.Name != "number" || len(file.Commands) != 3 || file.Commands[2].Declaration == nil {
+		t.Fatalf("commands = %#v", file.Commands)
+	}
+	assertFileSpans(t, file)
+}
+
 func TestVim9TypeAliasMissingPartsRecoverNextLine(t *testing.T) {
 	tests := []struct {
 		name      string
